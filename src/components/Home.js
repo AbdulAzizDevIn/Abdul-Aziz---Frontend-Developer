510f862253bac05ca2646d270c588a186c23c238
Abdul Aziz <azizskwwe@gmail.com>
2025-01-19 23:58:59 +0530

    Add new images and enhance Home component with additional sections and features

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,11 @@
 import investment from "../dataPNG/Investment.png";
 import rectangle from "../dataPNG/Rectangle 3 (1).png";
 import connector from "../dataPNG/Connector-pic.png";
+import readMore from "../dataPNG/read-more.png";
+import wallet from "../dataPNG/wallet.png";
+import tools from "../dataPNG/Tools.png";
+import graph from "../dataPNG/graph.png";
+import futureTech from "../dataPNG/Future Technology.png";
 export const Home = () => {
   return (
     <main className="relative z-10 text-white h-screen bg-opacity-50">
@@ -20,7 +25,7 @@ export const Home = () => {
         </button>
       </div>
 
-      <div id="Container" className="bg-[#00161d]">
+      <div id="Container-1" className="bg-[#00161d] ">
         <div className="pt-[80px]  flex  justify-center items-center p-[32px_120px_40px_120px] gap-[16px] self-stretch">
           <div className="flex justify-between rounded-tl-[56px] rounded-tr-[16px] rounded-br-[16px] rounded-bl-[16px] bg-[rgba(12,43,47,0.7)] w-[280px] h-[504px] p-[72px_37px_32px_37px] flex-col items-center shrink-0flex w-[280px] h-[504px] p-[72px_37px_32px_37px] flex-col items-center shrink-0 border-[1px] border-[#0F373C] hover:shadow-[inset_0px_1px_25px_0px_rgba(29,206,242,0.25)]  transition-shadow duration-300">
             <div>
@@ -69,9 +74,98 @@ export const Home = () => {
             </div>
           </div>
         </div>
-        <div className="flex w-[1200px] h-[240px] justify-center items-center gap-[16px]">
-
+        <div className="flex justify-center items-center px-[120px] py-[32px] pb-[40px] gap-[16px] self-stretch">
+          <div className="flex flex-col w-[446px] h-[240px] p-[32px] items-start gap-[8px] flex-shrink-0 rounded-tl-[16px] rounded-tr-[16px] rounded-br-[16px] rounded-bl-[56px] bg-[rgba(12,43,47,0.70)] border-[1px] border-[#0F373C] bg-[rgba(12,43,47,0.7)] hover:shadow-[inset_0px_1px_25px_0px_rgba(29,206,242,0.25)]  transition-shadow duration-300">
+            <h3 className="text-white text-[22px] font-medium leading-[26px] tracking-[-0.44px]">
+              Portfolio Sync
+            </h3>
+            <p className="text-[rgba(176,250,255,0.7)] text-left text-[16px] font-normal leading-[24px]">
+              Easily manage your portfolio. <br /> You'll always know what you{" "}
+              <br /> own, how it's performing, and <br /> where it’s headed.
+            </p>
+          </div>
+          <div className="flex flex-col h-[240px] w-[700px] p-[32px] items-start gap-[16px] rounded-tl-[16px] rounded-tr-[16px] rounded-br-[56px] rounded-bl-[16px] bg-[rgba(12,43,47,0.7)] border-[1px] border-[#0F373C] bg-[rgba(12,43,47,0.7)] hover:shadow-[inset_0px_1px_25px_0px_rgba(29,206,242,0.25)]  transition-shadow duration-300">
+            <h3 className="text-white text-[22px] font-medium leading-[26px] tracking-[-0.44px]">
+              Opportunity Scout
+            </h3>
+            <p className="text-[rgba(176,250,255,0.7)] text-left text-[16px] font-normal leading-[24px] ">
+              Find exciting new projects, events and <br /> tokens that others
+              might be missing. <br /> Identifying promising opportunities{" "}
+              <br /> early, so you never miss out on the <br /> next big thing.
+            </p>
+          </div>
         </div>
+      </div>
+
+      <div
+        id="Container-2"
+        className="bg-[#00161d] flex flex-col pt-[100px] justify-center items-center self-stretch"
+      >
+        <h2 className="text-white text-center font-space-grotesk text-[40px] font-medium leading-[44px] tracking-[-0.8px]">
+          About EthAi
+        </h2>
+        <p className="pt-5 pb-5 text-[#B0FAFFB2] text-center font-space-grotesk text-[16px] font-normal leading-[24px]">
+          At EthAi, we’re all about making crypto trading easier and more
+          intuitive. We provide tools <br /> that help traders keep up with all
+          new market trends, track top traders’ movements.
+        </p>
+        <img className="cursor-pointer" src={readMore} alt="read-more" />
+
+        <div className="flex flex-col gap-[40px] w-[744px] h-[400px] p-[48px_64px_48px_63px] justify-center items-center rounded-[20px] bg-[#08252A]">
+          <div className="flex items-center gap-[36px] self-stretch">
+            <div className="flex flex-col items-start gap-[12px] flex-1">
+              <img src={graph} alt="" />
+              <h5 className="flex flex-col justify-center self-stretch text-white text-[18px] font-[500] leading-[24px] tracking-[-0.36px]">
+                Stay Ahead
+              </h5>
+              <p className="self-stretch text-[rgba(176,250,255,0.70)] font-[400] text-[16px] leading-[24px]">
+                No more guesswork—get clear, trustable insights.
+              </p>
+            </div>
+
+            <div className="flex flex-col items-start gap-[12px] flex-1">
+              <div className="flex flex-col items-start gap-[12px] flex-1">
+                <img src={wallet} alt="" />
+                <h5 className="flex flex-col justify-center self-stretch text-white text-[18px] font-[500] leading-[24px] tracking-[-0.36px]">
+                  Know Your Assets
+                </h5>
+                <p className="self-stretch text-[rgba(176,250,255,0.70)] font-[400] text-[16px] leading-[24px]">
+                  Always stay on top of how your investments are performing.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-[36px] self-stretch">
+            <div className="flex flex-col items-start gap-[12px] flex-1">
+              <img src={tools} alt="" />
+              <h5 className="flex flex-col justify-center self-stretch text-white text-[18px] font-[500] leading-[24px] tracking-[-0.36px]">
+                Simple, Not Overwhelming
+              </h5>
+              <p className="self-stretch text-[rgba(176,250,255,0.70)] font-[400] text-[16px] leading-[24px]">
+                Our tools are designed to make complex market analysis easy to
+                understand and act on.
+              </p>
+            </div>
+
+            <div className="flex flex-col items-start gap-[12px] flex-1">
+              <div className="flex flex-col items-start gap-[12px] flex-1">
+                <img src={futureTech} alt="" />
+                <h5 className="flex flex-col justify-center self-stretch text-white text-[18px] font-[500] leading-[24px] tracking-[-0.36px]">
+                  Future-Proof
+                </h5>
+                <p className="self-stretch text-[rgba(176,250,255,0.70)] font-[400] text-[16px] leading-[24px]">
+                  We’re constantly improving, adding new features to help you
+                  make the most informed decisions possible.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="Container-3" className="bg-[#00161d] flex flex-col justify-center items-center self-stretch">
+        
       </div>
     </main>
   );
